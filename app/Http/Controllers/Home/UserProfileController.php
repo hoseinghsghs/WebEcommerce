b4b5c1a810925e96504536513d0c3d1ef9821208
rajabi-amir <73391648+rajabi-amir@users.noreply.github.com>
2022-09-11 23:47:03 +0430

    make edit user profile

--- app/Http/Controllers/Home/UserProfileController.php
+++ app/Http/Controllers/Home/UserProfileController.php
@@ -11,32 +11,33 @@ class UserProfileController extends Controller
 {
     public function index()
     {
-        
+
         $comments= Comment::where('user_id', auth()->id())->where('approved' , 1)->get();
         return view('home.page.users_profile.index' , compact('comments'));
     }
 
-    public function orderList() 
+    public function orderList()
     {
         $orders=Order::where('user_id', auth()->id())->get();
         return view('home.page.users_profile.order.orderList' , compact('orders'));
     }
 
-    public function order(Order $order) 
+    public function order(Order $order)
     {
         return view('home.page.users_profile.order.show' , compact('order'));
-    } 
+    }
 
-     public function commentsList() 
+     public function commentsList()
     {
         $comments= Comment::where('user_id', auth()->id())->get();
 
         return view('home.page.users_profile.comments' , compact('comments'));
-    } 
-    
-    public function editProfile() 
+    }
+
+    public function editProfile()
     {
-        return view('home.page.users_profile.editProfile');
+        $user=auth()->user();
+        return view('home.page.users_profile.editProfile',compact('user'));
     }
 
 }
--- resources/views/home/layout/MasterHome.blade.php
+++ resources/views/home/layout/MasterHome.blade.php
@@ -22,92 +22,104 @@
 
     <script type="text/javascript" src="{{asset('js/home.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightgallery/1.3.2/js/lightgallery.js"></script>
-
     <script>
-    function number_format(number, decimals, dec_point, thousands_sep) {
-        // Strip all characters but numerical ones.
-        number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
-        var n = !isFinite(+number) ? 0 : +number,
-            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
-            sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
-            dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
-            s = '',
-            toFixedFix = function(n, prec) {
-                var k = Math.pow(10, prec);
-                return '' + Math.round(n * k) / k;
-            };
-        // Fix for IE parseFloat(0.55).toFixed(0) = 0;
-        s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
-        if (s[0].length > 3) {
-            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
-        }
-        if ((s[1] || '').length < prec) {
-            s[1] = s[1] || '';
-            s[1] += new Array(prec - s[1].length + 1).join('0');
-        }
-        return s.join(dec);
-    }
-
-    function delete_product_cart(id) {
-
-        let url =
-            window.location.origin +
-            "/remove-from-cart" +
-            "/" + id;
+        // show message if exist
+        @if (request()->session()->has('message'))
         Swal.fire({
-            text: "آیا مطمئن هستید حذف شود؟",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "بله",
-            cancelButtonText: "خیر",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $.get(url,
-                    function(response, status, xyz) {
-                        console.log(status);
-                        if (status == 'success') {
-                            let price = number_format(response);
-                            $(".price-total").html(price + ' ' + 'تومان');
-                            $("#" + id).remove();
-                            $("#shopping-bag-item").html(
-                                parseInt($("#shopping-bag-item").html(), 10) - 1
-                            );
-                            $("#count-cart").html(
-                                parseInt($("#count-cart").html(), 10) - 1
-                            );
-
-                            Livewire.emit('delete', id);
-                        }
-                    }).fail(function() {
-                    Swal.fire({
-                        title: "مشکل",
-                        text: " اینترنت شما قطع است",
-                        icon: "error",
-                        timer: 1500,
-                        ConfirmButton: "باشه",
-
-                    });
-                })
-            } else {
-
-            }
+        text: "{{session('message.text')}}",
+        icon: "{{session('message.type')}}",
+        showConfirmButton: false,
+        toast: true,
+        position: 'top-right',
+        timer: 5000,
+        timerProgressBar: true,
         });
-    }
-    window.addEventListener('say-goodbye', event => {
-        $("#shopping-bag-item").html(
-            parseInt($("#shopping-bag-item").html(), 10) - 1
-        );
-        $("#count-cart").html(
-            parseInt($("#count-cart").html(), 10) - 1
-        );
+        @endif
+        //end show messsage
+        function number_format(number, decimals, dec_point, thousands_sep) {
+            // Strip all characters but numerical ones.
+            number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+            var n = !isFinite(+number) ? 0 : +number,
+                prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+                sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+                dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+                s = '',
+                toFixedFix = function(n, prec) {
+                    var k = Math.pow(10, prec);
+                    return '' + Math.round(n * k) / k;
+                };
+            // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+            s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+            if (s[0].length > 3) {
+                s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+            }
+            if ((s[1] || '').length < prec) {
+                s[1] = s[1] || '';
+                s[1] += new Array(prec - s[1].length + 1).join('0');
+            }
+            return s.join(dec);
+        }
 
-        $("#" + event.detail.rowId).remove();
+        function delete_product_cart(id) {
 
-        $(".mini-card-total").remove();
+            let url =
+                window.location.origin +
+                "/remove-from-cart" +
+                "/" + id;
+            Swal.fire({
+                text: "آیا مطمئن هستید حذف شود؟",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "بله",
+                cancelButtonText: "خیر",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    $.get(url,
+                        function(response, status, xyz) {
+                            console.log(status);
+                            if (status == 'success') {
+                                let price = number_format(response);
+                                $(".price-total").html(price + ' ' + 'تومان');
+                                $("#" + id).remove();
+                                $("#shopping-bag-item").html(
+                                    parseInt($("#shopping-bag-item").html(), 10) - 1
+                                );
+                                $("#count-cart").html(
+                                    parseInt($("#count-cart").html(), 10) - 1
+                                );
 
-    });
+                                Livewire.emit('delete', id);
+                            }
+                        }).fail(function() {
+                        Swal.fire({
+                            title: "مشکل",
+                            text: " اینترنت شما قطع است",
+                            icon: "error",
+                            timer: 1500,
+                            ConfirmButton: "باشه",
+
+                        });
+                    })
+                } else {
+
+                }
+            });
+        }
+        window.addEventListener('say-goodbye', event => {
+            $("#shopping-bag-item").html(
+                parseInt($("#shopping-bag-item").html(), 10) - 1
+            );
+            $("#count-cart").html(
+                parseInt($("#count-cart").html(), 10) - 1
+            );
+
+            $("#" + event.detail.rowId).remove();
+
+            $(".mini-card-total").remove();
+
+        });
     </script>
 
     @include('sweetalert::alert')
@@ -128,37 +140,37 @@
 
 
 <script>
-$(document).ready(function() {
+    $(document).ready(function() {
 
-    $(".carousel").carousel({
-        interval: false,
-        pause: true,
-        touch: true
+        $(".carousel").carousel({
+            interval: false,
+            pause: true,
+            touch: true
+        });
+
+        $(".carousel .carousel-inner").swipe({
+            swipeLeft: function(event, direction, distance, duration, fingerCount) {
+                this.parent().carousel("next");
+            },
+            swipeRight: function() {
+                this.parent().carousel("prev");
+            },
+            threshold: 0,
+            tap: function(event, target) {
+                window.location = $(this).find(".carousel-item.active a").attr("href");
+            },
+            excludedElements: "label, button, input, select, textarea, .noSwipe"
+        });
+
+        $(".carousel .carousel-control-prev").on("click", function() {
+            $(".carousel").carousel("prev");
+        });
+
+        $(".carousel .carousel-control-next").on("click", function() {
+            $(".carousel").carousel("next");
+        });
+
     });
-
-    $(".carousel .carousel-inner").swipe({
-        swipeLeft: function(event, direction, distance, duration, fingerCount) {
-            this.parent().carousel("next");
-        },
-        swipeRight: function() {
-            this.parent().carousel("prev");
-        },
-        threshold: 0,
-        tap: function(event, target) {
-            window.location = $(this).find(".carousel-item.active a").attr("href");
-        },
-        excludedElements: "label, button, input, select, textarea, .noSwipe"
-    });
-
-    $(".carousel .carousel-control-prev").on("click", function() {
-        $(".carousel").carousel("prev");
-    });
-
-    $(".carousel .carousel-control-next").on("click", function() {
-        $(".carousel").carousel("next");
-    });
-
-});
 </script>
 
 
