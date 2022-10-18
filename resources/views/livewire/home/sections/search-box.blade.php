3e3078218a206e68babea4a3f5a267977db3bb30
hoseinghsghs <44768710+hoseinghsghs@users.noreply.github.com>
2022-10-18 15:08:38 +0330

    style

--- resources/views/livewire/home/sections/search-box.blade.php
+++ resources/views/livewire/home/sections/search-box.blade.php
@@ -3,9 +3,10 @@
         <div class="form1">
             <i class="fa fa-search" wire:click="search" wire:loading.remove></i>
             <i class="mdi mdi-loading mdi-spin" wire:loading></i>
-            <input type="text" class="form-control form-input" value="{{session('search')??''}}" wire:model.debounce.500ms="search" placeholder="محصول خود را جستجو کنید...">
+            <input type="text" class="form-control form-input" value="{{session('search')??''}}"
+                wire:model.debounce.500ms="search" placeholder="محصول خود را جستجو کنید...">
             <span class="left-pan1">
-                <select class="custom-select border-0" wire:model="categoryId">
+                <select class="custom-select border-0" id="search-input" wire:model="categoryId">
                     <option value="">همه دسته ها</option>
                     @foreach($categories as $category)
                     <option value="{{$category->id}}">{{$category->name}}</option>
@@ -19,8 +20,10 @@
         <ul class="search-result-list mb-0">
             @forelse ($sProducts as $product )
             <li>
-                <a class="d-flex align-items-center border-bottom border-light" href="{{route('home.products.show',$product->slug)}}"><i class="mdi mdi-clock-outline"></i>
-                    <img src="{{asset('storage/primary_image/'.$product->primary_image)}}" alt="image" width="60" height="60" class="suggestion-image border rounded">
+                <a class="d-flex align-items-center border-bottom border-light"
+                    href="{{route('home.products.show',$product->slug)}}"><i class="mdi mdi-clock-outline"></i>
+                    <img src="{{asset('storage/primary_image/'.$product->primary_image)}}" alt="image" width="60"
+                        height="60" class="suggestion-image border rounded">
                     <span class="mr-2">{{$product->name}}</span>
                     <span class="mr-auto ml-1">{{$product->category->parent->name}} /
                         {{$product->category->name}}</span>
