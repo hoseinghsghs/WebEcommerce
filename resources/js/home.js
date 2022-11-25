a954d2e9dde87f182e46343809e2be6599916c61
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2022-11-25 12:31:07 +0330

    websoket 03

--- resources/js/home.js
+++ resources/js/home.js
@@ -23,15 +23,15 @@ require("../assets/home/js/vendor/lightgallery.js");
 require("../assets/home/js/vendor/jquery.ez-plus.js");
 require("../assets/home/js/main.js");
 
-import Echo from "laravel-echo";
+//import Echo from "laravel-echo";
 
-window.Pusher = require("pusher-js");
+//window.Pusher = require("pusher-js");
 
-window.Echo = new Echo({
-    broadcaster: "pusher",
-    key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    forceTLS: false,
-    disableStats: true,
-});
+//window.Echo = new Echo({
+//    broadcaster: "pusher",
+//    key: process.env.MIX_PUSHER_APP_KEY,
+//    wsHost: window.location.hostname,
+//    wsPort: 6001,
+//    forceTLS: false,
+//   disableStats: true,
+//});
