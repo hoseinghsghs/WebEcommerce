7a79724393c3783836b0ca321f60e8503038de54
likoshop <admin@likoshop.ir>
2022-11-28 20:33:51 +0330

    from server

--- resources/views/home/partial/Adplacement.blade.php
+++ resources/views/home/partial/Adplacement.blade.php
@@ -1,9 +1,9 @@
 <!-- adplacement--------------------------->
 <div class="container-fluid">
     <div class="row">
-        <div class="adplacement-container-row ml-2 my-1">
+        <div class="adplacement-container-row ml-2 my-0">
             @foreach ($headers as $header )
-            <div class="col-6 col-lg-3 p-1 pr">
+            <div class="col-4 col-lg-2 p-1 pr">
                 <a href="{{$header->link}}" class="adplacement-item m-1 ">
                     <div class="adplacement-sponsored_box">
                         <img src="{{url(env('BANNER_IMAGES_PATCH').$header->image)}}" alt="{{$header->title}}">
