c61b206a7375c3fca33ef47cce2e36ebfabd3c41
Amir <73391648+rajabi-amir@users.noreply.github.com>
2022-12-01 15:52:18 +0330

    fix product category

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -227,7 +227,7 @@ class ProductController extends Controller
     //دسته بندی
     public function editCategory(Request $request, Product $product)
     {
-        $categories = Category::where('parent_id', '!=', 0)->orderBy('parent_id')->get();
+        $categories = Category::with('children.children')->get();
         return view('admin.page.products.edit_category', compact('product', 'categories'));
     }
 
