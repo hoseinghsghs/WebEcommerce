2d77e38884e0a4dec1604f3c4fc31e20f03f11c2
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2022-12-07 20:00:28 +0330

    gufgjhtfg

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -35,42 +35,42 @@ class GenerateSitemap extends Command
 
         $sitemap = Sitemap::create();
 
-        // $sitemap->add(Url::create('/')->setPriority(1.0));
-        // $sitemap->add(Url::create('/faq')->setPriority(0.8));
-        // $sitemap->add(Url::create('/privacy')->setPriority(0.8));
-        // $sitemap->add(Url::create('/rules')->setPriority(0.8));
+        $sitemap->add(Url::create('/')->setPriority(1.0));
+        $sitemap->add(Url::create('/faq')->setPriority(0.8));
+        $sitemap->add(Url::create('/privacy')->setPriority(0.8));
+        $sitemap->add(Url::create('/rules')->setPriority(0.8));
 
-        // Product::active()->get()->each(function (Product $product) use ($sitemap) {
-        //     $sitemap->add(Url::create("/products/{$product->slug}")
-        //     ->setPriority(0.9)
-        //     );
-        // });
-        // Category::where('parent_id' , 0)->where('is_active', 1)->get()->each(function (Category $category) use ($sitemap) {
-        //     $sitemap->add(Url::create("/search/{$category->slug}")
-        //     ->setPriority(0.7)
-        //     );
-        // });
-        // Tag::all()->each(function (Tag $tag) use ($sitemap) {
-        //     $sitemap->add(Url::create("/search/?tag={$tag->name}")
-        //     ->setPriority(0.8)
-        //     );
-        // });
-        // Category::where('parent_id' , '!=' , 0)->where('is_active', 1)->get()->each(function (Category $category) use ($sitemap) {
-        //     $sitemap->add(Url::create("/main/{$category->slug}")
-        //     ->setPriority(0.7)
-        //     );
-        // });
-        // Post::where('status' , 1)->select('category')->distinct()->get()->each(function (Post $post) use ($sitemap) {
-        //     $sitemap->add(Url::create("/post/list/{$post->category}")
-        //     ->setPriority(0.7)
-        //     );
-        // });
+        Product::active()->get()->each(function (Product $product) use ($sitemap) {
+            $sitemap->add(Url::create("/products/{$product->slug}")
+            ->setPriority(0.9)
+            );
+        });
+        Category::where('parent_id' , 0)->where('is_active', 1)->get()->each(function (Category $category) use ($sitemap) {
+            $sitemap->add(Url::create("/search/{$category->slug}")
+            ->setPriority(0.7)
+            );
+        });
+        Tag::all()->each(function (Tag $tag) use ($sitemap) {
+            $sitemap->add(Url::create("/search/?tag={$tag->name}")
+            ->setPriority(0.8)
+            );
+        });
+        Category::where('parent_id' , '!=' , 0)->where('is_active', 1)->get()->each(function (Category $category) use ($sitemap) {
+            $sitemap->add(Url::create("/main/{$category->slug}")
+            ->setPriority(0.7)
+            );
+        });
+        Post::where('status' , 1)->select('category')->distinct()->get()->each(function (Post $post) use ($sitemap) {
+            $sitemap->add(Url::create("/post/list/{$post->category}")
+            ->setPriority(0.7)
+            );
+        });
 
-        // Post::where('status' , 1)->get()->each(function (Post $post) use ($sitemap) {
-        //     $sitemap->add(Url::create("/post/{$post->slug}")
-        //     ->setPriority(0.7)
-        //     );
-        // });
+        Post::where('status' , 1)->get()->each(function (Post $post) use ($sitemap) {
+            $sitemap->add(Url::create("/post/{$post->slug}")
+            ->setPriority(0.7)
+            );
+        });
         
         $sitemap->writeToFile(public_path('sitemap.xml'));
       
