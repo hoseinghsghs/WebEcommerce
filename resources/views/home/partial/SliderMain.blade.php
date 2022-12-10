9143d40ef50c092b6b0b63e0feb451044b20c329
Amir <73391648+rajabi-amir@users.noreply.github.com>
2022-12-10 14:42:16 +0330

    test

--- resources/views/home/partial/SliderMain.blade.php
+++ resources/views/home/partial/SliderMain.blade.php
@@ -22,7 +22,7 @@
                                     <picture>
                                         <source srcset="{{url(env('BANNER_IMAGES_PATCH').$slider->image)}}" media="(min-width: 970px)">
                                         <source srcset="{{route('fly-manipulation',['name'=>$slider->image,'width'=>1750,'height'=>700])}}" media="(min-width: 768px)">
-                                        <source srcset="{{route('fly-manipulation',['name'=>$slider->image,'width'=>1500,'height'=>500])}}" media="(min-width: 270px)">
+                                        <source srcset="{{route('fly-manipulation',['name'=>$slider->image,'width'=>2880,'height'=>500])}}" media="(min-width: 270px)">
                                         <img class="d-block w-100 h-four-img" srcset="{{url(env('BANNER_IMAGES_PATCH').$slider->image)}}" alt="…">
                                     </picture>
                                     {{--<img src="{{route('fly-manipulation',['name'=>$slider->image,'width'=>360,'height'=>270])}}"
