fcbe96ca9f4cd6eb30237a996d8ede3780d0b7f9
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2022-12-16 08:25:12 +0330

    لباب

--- app/PaymentGateway/mellat/nusoap.php
+++ app/PaymentGateway/mellat/nusoap.php
@@ -68,12 +68,12 @@ require_once('class.wsdl.php');
 
 // server class
 */
-require_once('class.soapclient.php');
-require_once('class.soap_server.php');
+
 // class variable emulation
 // cf. http://www.webkreator.com/php/techniques/php-static-class-variables.html
 $GLOBALS['_transient']['static']['nusoap_base']['globalDebugLevel'] = 9;
-
+require_once('class.soapclient.php');
+require_once('class.soap_server.php');
 /**
 *
 * nusoap_base
