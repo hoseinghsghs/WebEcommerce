a614bd0b7fbffe4eac92ab6abefd0de0428dc1ec
Amir <73391648+rajabi-amir@users.noreply.github.com>
2022-12-17 21:54:34 +0330

    fix mellat

--- app/Http/Controllers/Home/PaymentController.php
+++ app/Http/Controllers/Home/PaymentController.php
@@ -111,7 +111,7 @@ class PaymentController extends Controller
                 return redirect()->back();
             } else {
                 Session::put('orderId', $payGatewayResult['orderId']);
-//                dd($payGatewayResult,$payGatewayResult['orderId'],Session::get('orderId'));
+                dd($payGatewayResult,$payGatewayResult['orderId'],Session::get('orderId'));
                 echo "<form name='myform' action='https://bpm.shaparak.ir/pgwchannel/startpay.mellat' method='POST'><input type='hidden' id='RefId' name='RefId' value='{$payGatewayResult['success']}'></form><script type='text/javascript'>window.onload = formSubmit; function formSubmit() { document.forms[0].submit(); }</script>";
                 return true;
             }
