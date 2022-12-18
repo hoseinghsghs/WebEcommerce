e51c68452f595476a38de0a20ddfe91a810624b0
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2022-12-18 15:28:54 +0330

    jbk

--- app/PaymentGateway/Mellat.php
+++ app/PaymentGateway/Mellat.php
@@ -175,7 +175,7 @@ class Mellat extends Payment
 			echo "<form name='myform' action='https://bpm.shaparak.ir/pgwchannel/startpay.mellat' method='POST'><input type='hidden' id='RefId' name='RefId' value='{$refIdValue}'></form><script type='text/javascript'>window.onload = formSubmit; function formSubmit() { document.forms[0].submit(); }</script>";
 	}
     public function checkPayment($RefId, $ResCode, $SaleOrderId, $SaleReferenceId)
-    {       dd(auth()->id());
+    {  
         if ($ResCode == 0) {
              
             if ($this->verify($RefId, $ResCode, $SaleOrderId, $SaleReferenceId) == true) {
