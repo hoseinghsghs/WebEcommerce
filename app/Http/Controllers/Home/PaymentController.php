961462ae7c87046b3fb0331385f658da1e2f6ee4
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2022-12-18 14:50:15 +0330

    kiughi

--- app/Http/Controllers/Home/PaymentController.php
+++ app/Http/Controllers/Home/PaymentController.php
@@ -112,6 +112,7 @@ class PaymentController extends Controller
                 return redirect()->back();
             } else {
                 echo "<form name='myform' action='https://bpm.shaparak.ir/pgwchannel/startpay.mellat' method='POST'><input type='hidden' id='RefId' name='RefId' value='{$payGatewayResult['success']}'></form><script type='text/javascript'>window.onload = formSubmit; function formSubmit() { document.forms[0].submit(); }</script>";
+                     dd(auth()->id());
                 return true;
             }
         }
