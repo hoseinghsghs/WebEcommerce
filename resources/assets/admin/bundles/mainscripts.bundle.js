8afd7245005a834388e0338a252890c7d98e318b
Amir <73391648+rajabi-amir@users.noreply.github.com>
2022-12-19 18:11:22 +0330

    fix mellat & sidebar

--- resources/assets/admin/bundles/mainscripts.bundle.js
+++ resources/assets/admin/bundles/mainscripts.bundle.js
@@ -20,7 +20,7 @@ function skinChanger() {
 }
 function CustomScrollbar() {
     $(".sidebar .menu .list").slimscroll({
-        height: "calc(112vh - 213px)",
+        height: "calc(112vh - 230px)",
         color: "#eeeeee",
         position: "right",
         size: "1px",
