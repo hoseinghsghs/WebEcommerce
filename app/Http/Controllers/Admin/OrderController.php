9609c56425991d3dbab93bd0e71af9967ff2eb83
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2022-12-20 20:27:31 +0330

    sdfsd

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -87,7 +87,7 @@ class OrderController extends Controller
             try {
                 Notification::route('cellphone', '09139035692')->notify(new InvoicePaid(["code" => $order->user->cellphone . "زرین پال سفارش جدید دارید"], "j3tlqr5zjecppsf", ['09139035692', '09162418808']));
             } catch (\Throwable $th) {
-                //throw $th;
+                dd($th);
             }
         };
         $flasher->addSuccess('سفارش ویرایش شد.');
