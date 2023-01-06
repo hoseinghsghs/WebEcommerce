efc084fa04d4669adcc6f82840808521a50b3244
hosein <44768710+hoseinghsghs@users.noreply.github.com>
2023-01-06 21:55:02 +0330

    link

--- resources/views/home/partial/Adplacement.blade.php
+++ resources/views/home/partial/Adplacement.blade.php
@@ -4,7 +4,7 @@
         <div class="adplacement-container-row ml-2 my-0">
             @foreach ($headers as $header )
             <div class="col-4 col-lg-2 p-1 pr">
-                <a href="{{$header->link}}" class="adplacement-item m-1 ">
+                <a href="{{$header->button_link}}" class="adplacement-item m-1 ">
                     <div class="adplacement-sponsored_box">
                         <img src="{{url(env('BANNER_IMAGES_PATCH').$header->image)}}" alt="{{$header->title}}">
                     </div>
